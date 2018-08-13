094b50cd09d614e60d34c3b0c6fa63359415c066
John Jairo Tellez <jtellezm1976@universidadean.edu.co>
2018-08-13 12:07:30 -0500

    Ejercicio1 Terminado

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -13,39 +13,99 @@ public class Ejercicio1
        // Variables
        double Numero, Pies, Centi, Leguas, Yardas;
        char Opcion;
-               
+       char Respuesta;       
        // Recoger datos
        System.out.println("CALCULO DE PIES, CENTIMETROS, LEGUAS Y YARDAS\n ");
        System.out.println("-------------------------------------------------------------------------------------------------------------------------------\n\n");
        
+       do {
+       
        System.out.print("Ingrese el numero a convertir:  ");
        Numero = teclado.nextDouble();
        
-       System.out.print("Ingrese la letra de unidad de conversion: P(Pies) C(Centimetros) L(Leguas) o Y(Yardas):  ");
+       System.out.print("Ingrese la letra de unidad: \n\n 1. Pies (p) \n\n 2. Centimetros (c) \n\n 3. Leguas (l) \n\n 4. Yardas (y) \n\n 5. Salir (s):  ");
        Opcion = teclado.next().charAt(0);
        
        System.out.println("-------------------------------------------------------------------------------------------------------------------------------\n\n");
        
-              Opcion = (char)(Opcion - 'a' + 'A');
+       Opcion = (char)(Opcion - 'a' + 'A');
               
        
        if (Opcion =='P'){
-            
-                System.out.print("Funcion P  ");
+           System.out.print("Convirtiendo " + Numero + " Pies en Pulgadas.\n\n");     
+           Pies = Numero / 0.0833;
+           System.out.print("Resultado  " + Math.round(Pies * 100d)/ 100d + " Pulgadas");
+           System.out.println("\n\n¿Desea realizar otra consulta? presione s/n ");
+           Respuesta = teclado.next().charAt(0);
+           if (Respuesta == 'n') {
+                        
+                            System.out.println("Saliendo, gracias.");
+                            System.exit(1);
+                        }
+                        if (Respuesta != 's' && Respuesta != 'n'){
+                            
+                            System.out.println("Opcion no valida...cerrando");
+                            System.exit(1);
+                        }
             };
        if (Opcion=='C'){
-            
-                System.out.print("Funcion C  ");
+           System.out.print("Convirtiendo " + Numero + " Centimetros en Pulgadas.\n\n");       
+           Centi = Numero * 0.393701;
+           System.out.print("Resultado  " + Math.round(Centi * 100d)/ 100d + " Pulgadas");
+           System.out.println("\n\n¿Desea realizar otra consulta? presione s/n ");
+           Respuesta = teclado.next().charAt(0);
+           if (Respuesta == 'n') {
+                        
+                            System.out.println("Saliendo, gracias.");
+                            System.exit(1);
+                        }
+                        if (Respuesta != 's' && Respuesta != 'n'){
+                            
+                            System.out.println("Opcion no valida...cerrando");
+                            System.exit(1);
+                        }
             };
        if (Opcion=='L'){
-            
-                System.out.print("Funcion L  ");
+           System.out.print("Convirtiendo " + Numero + " Leguas en Pulgadas.\n\n");     
+           Leguas = Numero * 190080;
+           System.out.print("Resultado  " + Math.round(Leguas * 100d)/ 100d + " Pulgadas");
+           System.out.println("\n\n¿Desea realizar otra consulta? presione s/n ");
+           Respuesta = teclado.next().charAt(0);
+           if (Respuesta == 'n') {
+                        
+                            System.out.println("Saliendo, gracias.");
+                            System.exit(1);
+                        }
+                        if (Respuesta != 's' && Respuesta != 'n'){
+                            
+                            System.out.println("Opcion no valida...cerrando");
+                            System.exit(1);
+                        }
             };
        if (Opcion=='Y'){
+           System.out.print("Convirtiendo " + Numero + " Yardas en Pulgadas.\n\n");     
+           Yardas = Numero * 36;
+           System.out.print("Resultado  " + Math.round(Yardas * 100d)/ 100d + " Pulgadas");
+           System.out.println("\n\n¿Desea realizar otra consulta? presione s/n ");
+           Respuesta = teclado.next().charAt(0);
+           if (Respuesta == 'n') {
+                        
+                            System.out.println("Saliendo, gracias.");
+                            System.exit(1);
+                        }
+                        if (Respuesta != 's' && Respuesta != 'n'){
+                            
+                            System.out.println("Opcion no valida...cerrando");
+                            System.exit(1);
+                        }
+            } 
             
-                System.out.print("Funcion Y  ");
-            };
-       
+       if (Opcion== 'S') {
+           System.out.println("Saliendo, gracias.");
+            System.exit(1);
+        }
+        } while (Opcion!='s');
+        }
       /*
        
        System.out.println("Resultados:\n");
@@ -60,5 +120,5 @@ public class Ejercicio1
        
       */ 
        
-    }
+    
 }
