96d124c2c894f562b607c458f07b6085e4b43ad2
DESKTOP-S2ALEVN\Familia Tellez <jtellezm1976@universidadean.edu.co>
2018-08-12 23:27:17 -0500

    V1.0

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * Write a description of class Ejercicio1 here.
  * 
@@ -7,27 +7,58 @@
  */
 public class Ejercicio1
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Ejercicio1
-     */
-    public Ejercicio1()
+    public static void main(String[] args)
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        Scanner teclado = new Scanner(System.in);
+       // Variables
+       double Numero, Pies, Centi, Leguas, Yardas;
+       char Opcion;
+               
+       // Recoger datos
+       System.out.println("CALCULO DE PIES, CENTIMETROS, LEGUAS Y YARDAS\n ");
+       System.out.println("-------------------------------------------------------------------------------------------------------------------------------\n\n");
+       
+       System.out.print("Ingrese el numero a convertir:  ");
+       Numero = teclado.nextDouble();
+       
+       System.out.print("Ingrese la letra de unidad de conversion: P(Pies) C(Centimetros) L(Leguas) o Y(Yardas):  ");
+       Opcion = teclado.next().charAt(0);
+       
+       System.out.println("-------------------------------------------------------------------------------------------------------------------------------\n\n");
+       
+              Opcion = (char)(Opcion - 'a' + 'A');
+              
+       
+       if (Opcion =='P'){
+            
+                System.out.print("Funcion P  ");
+            };
+       if (Opcion=='C'){
+            
+                System.out.print("Funcion C  ");
+            };
+       if (Opcion=='L'){
+            
+                System.out.print("Funcion L  ");
+            };
+       if (Opcion=='Y'){
+            
+                System.out.print("Funcion Y  ");
+            };
+       
+      /*
+       
+       System.out.println("Resultados:\n");
+       
+       System.out.println("Semiperimetro: " + Math.round(Sperimetro * 100d) / 100d + " cm"); //Redondear los decimales con Math.round()
+       
+       System.out.println("Area:  " + Math.round(Area * 100d) / 100d + " m2");
+       
+       System.out.println("Circumradius:  " + Cradius + " cm");
+       
+       System.out.println("-------------------------------------------------------------------------------------------------------------------------------\n\n");
+       
+      */ 
+       
     }
 }
